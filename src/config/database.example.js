83d225301f487978903404eedd52f139400a2c44
ardhian <ardhianwira@gmail.com>
2023-02-17 04:55:38 +0630

    seting db and env

--- src/config/database.example.js
+++ src/config/database.example.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize')
-var db = new Sequelize('express_sql', 'root', '', {
-    dialect: 'mysql',
-    host: 'localhost'
+var db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    dialect: process.env.DB_CONNECTION,
+    host: process.env.DB_HOST
 });
 
 
