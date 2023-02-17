27248525373f39f4e6bbd7224c47bae3e9a0e45e
PAPATIRI <syariftaufik11@gmail.com>
2023-02-17 22:27:43 +0700

    fix router supaya clean dan tambah model db.sync di index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,40 @@
-require('dotenv').config()
-const PORT = process.env.PORT || 5000
-const express = require("express")
-const cookieParser = require('cookie-parser');
-const usersRoutes = require("./routes/users.js")
-const middlewareLogReq = require("./middleware/logs")
-const upload = require('./middleware/multer.js')
-const app = express()
+require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const usersRoutes = require("./routes/users.js");
+const middlewareLogReq = require("./middleware/logs");
+const upload = require("./middleware/multer.js");
+const db = require("./config/database.js");
+const { Users } = require("./models/users.js");
+const app = express();
 
 app.use(cookieParser());
 
-app.use(middlewareLogReq)
-app.use(express.json())
-app.use('/assets', express.static('public/img'))
-app.use("/users", usersRoutes)
-app.post("/upload", upload.single('photo'), (req, res) => {
-    res.json({
-        msg: "Upload Success"
-    })
-})
+try {
+  db.authenticate();
+  console.log("database connected");
+  Users.sync();
+} catch (error) {
+  console.log(error);
+}
+
+app.use(middlewareLogReq);
+app.use(express.json());
+app.use("/assets", express.static("public/img"));
+app.use(usersRoutes);
+app.post("/upload", upload.single("photo"), (req, res) => {
+  res.json({
+    msg: "Upload Success",
+  });
+});
 
 app.use((err, req, res, next) => {
-    res.status(500).json({
-        msg: err.message
-    })
-})
-
+  res.status(500).json({
+    msg: err.message,
+  });
+});
 
 app.listen(PORT, () => {
-    console.log(`Server berhasil run di port ${PORT}`);
-})
+  console.log(`Server berhasil run di port ${PORT}`);
+});
